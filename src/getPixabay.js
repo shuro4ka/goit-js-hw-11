c92df39e6e802c0f2a282de4578ce08629ebf401
shuro4ka <zzlesia77@gmail.com>
2022-07-07 22:27:06 +0100

    add hw11

---
+++ src/getPixabay.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+const APIKEY = '28463198-9651460feed0dbf9f7cb6c698';
+const params = new URLSearchParams({
+    key: APIKEY,
+    q: 'cat',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+});
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+// axios.defaults.headers.common['key'] = APIKEY;
+
+export class GetPixabayAPI {
+    constructor() { 
+        this.searchQuery = ''; 
+        this.page = 1; 
+    }
+
+    async fetchImages(){
+        const params = new URLSearchParams({
+            key: APIKEY,
+            q: this.searchQuery,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+            page: this.page,
+            per_page: 40,
+        });
+        
+        const { data } = await axios.get(`?${params}`);
+        this.incrementPage();
+        return data;
+    }
+
+    get searchQueryy() {
+        return this.searchQuery;
+    }
+    set searchQueryy(newSearchQuery){
+        this.searchQuery = newSearchQuery;
+    }
+
+    incrementPage(){
+        this.page +=1;
+    }
+    resetPage(){
+        this.page = 1;
+    }
+}
+
---
+++ src/index.js
@@ -0,0 +1,104 @@
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { GetPixabayAPI } from './getPixabay';
+import axios from 'axios';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryRef = document.querySelector('.gallery');
+const formRef = document.querySelector('.search-form');
+const loadMoreBtnRef = document.querySelector('.load-more');
+const APIKEY = '28463198-9651460feed0dbf9f7cb6c698';
+const getPixabayAPI = new GetPixabayAPI();
+
+getPixabayAPI.fetchImages();
+formRef.addEventListener('submit', onFormSubmit);
+loadMoreBtnRef.addEventListener('click', onLoadMoreBtnClick);
+
+
+function makeGalleryMarkup(searchedImages) {
+    return searchedImages.map(({ 
+        webformatURL, 
+        largeImageURL, 
+        tags, 
+        likes, 
+        views, 
+        comments, 
+        downloads, 
+    }) => 
+  `<div class="photo-card">
+    <a href="${largeImageURL}">
+    <img src=${webformatURL} alt= ${tags} loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes: ${likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views: ${views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments:${comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads:${downloads}</b>
+      </p></a>
+    </div>
+  </div>`).join("");
+   
+}
+function renderGallery(searchedImages){
+    galleryRef.insertAdjacentHTML('beforeend', makeGalleryMarkup(searchedImages));
+}
+
+async function onFormSubmit(e){
+    e.preventDefault();
+    clearGalleryMarkup();
+    getPixabayAPI.resetPage();
+    const request = e.target.elements.searchQuery.value.trim();
+    if(!request) return Notify.info("Input the text");
+
+    getPixabayAPI.searchQueryy = request;
+    try {
+    const { hits, totalHits } = await getPixabayAPI.fetchImages();
+    
+    if(!totalHits)
+    return Notify.warning(
+      "Sorry, there are no images matching your search query. Please try again."
+      );
+    Notify.success(`Hooray! We found ${ totalHits } images.`);
+    renderGallery(hits);
+    lightbox.refresh();
+  } catch(err) { 
+      console.log(err.message);
+    }
+    e.target.reset();    
+  }
+
+async function onLoadMoreBtnClick(){
+  try {
+  const { hits, totalHits } = await getPixabayAPI.fetchImages();
+  renderGallery(hits);
+  lightbox.refresh();
+
+  const { height: cardHeight } = document
+  .querySelector(".gallery")
+  .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+});
+
+} catch (err) {
+  console.log(err.message);
+}
+}
+function clearGalleryMarkup() {
+  galleryRef.innerHTML = '';
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+  captionsData: 'alt',
+  captionDelay: 300,
+});
+
